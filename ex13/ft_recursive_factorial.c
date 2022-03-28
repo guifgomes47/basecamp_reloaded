1fddb6941f39c4f64743e6c0681e758d80bba5aa
guifgomes47 <guifgomes16@gmail.com>
2022-03-28 12:58:44 -0300

    ex13

---
+++ ex13/ft_recursive_factorial.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: guilhfer <guilhfer@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/26 13:33:01 by guilhfer          #+#    #+#             */
+/*   Updated: 2022/03/28 12:56:44 by guilhfer         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_recursive_factorial(int nb)
+{
+	if (nb < 0)
+	{
+		return (0);
+	}
+	if (nb <= 1)
+	{
+		return (1);
+	}
+	return (nb * ft_recursive_factorial(nb - 1));
+}
