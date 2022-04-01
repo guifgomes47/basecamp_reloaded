aeb79e6dd4aa6a3ce55be45e24cb5f0282c0e107
guifgomes47 <guifgomes16@gmail.com>
2022-04-01 15:33:23 -0300

    ex20

---
+++ ex20/ft_strdup.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: guilhfer <guilhfer@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/01 11:27:31 by guilhfer          #+#    #+#             */
+/*   Updated: 2022/04/01 15:31:33 by guilhfer         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "stdlib.h"
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	i++;
+	return (i);
+}
+
+char	*ft_strcpy(char *dest, char *src)
+{
+	char	*cpy;
+
+	cpy = dest;
+	while (*src)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	return (cpy);
+}
+
+char	*ft_strdup(char *src)
+{
+	char	*cpy;
+
+	cpy = malloc(ft_strlen(src) + 1);
+	if (cpy == 0)
+	{
+		return (0);
+	}
+	ft_strcpy(cpy, src);
+	return (cpy);
+}
