7c8b8e51bf530fddf39529ba6e902844e8217205
guifgomes47 <guifgomes16@gmail.com>
2022-04-03 17:31:03 -0300

    ex19fix

--- ex19/ft_sort_params.c
+++ ex19/ft_sort_params.c
@@ -6,17 +6,24 @@
 /*   By: guilhfer <guilhfer@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/29 16:03:43 by guilhfer          #+#    #+#             */
-/*   Updated: 2022/03/31 15:22:41 by guilhfer         ###   ########.fr       */
+/*   Updated: 2022/04/03 17:30:47 by guilhfer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_putchar(int c);
+#include <unistd.h>
+#include <stdio.h>
+
+void	ft_putchar(int c)
+{
+	write(1, &c, 1);
+}
 
 int	ft_strcmp(char *s1, char *s2)
 {
 	while (*s1 == *s2)
 	{
-		return (0);
+		s1++;
+		s2++;
 	}
 	return (*s1 - *s2);
 }
@@ -27,18 +34,17 @@ void	ft_swap(int argc, char *argv[])
 	int		j;
 	char	*temp;
 
-	temp = "";
-	i = 0;
+	i = 1;
 	while (i < argc)
 	{
-		j = 1;
-		while (j < argc - i - 1)
+		j = i + 1;
+		while (j < argc)
 		{
-			if (ft_strcmp(argv[j], argv[j + 1]) > 0)
+			if (ft_strcmp(argv[i], argv[j]) > 0)
 			{
-				temp = argv[j];
-				argv[j] = argv[j + 1];
-				argv[j + 1] = temp;
+				temp = argv[i];
+				argv[i] = argv[j];
+				argv[j] = temp;
 			}
 			j++;
 		}
