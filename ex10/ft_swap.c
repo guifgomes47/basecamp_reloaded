8ca3e71122a682daaae2a6748c5c0e5e9cef854d
guifgomes47 <guifgomes16@gmail.com>
2022-04-03 17:34:11 -0300

    fix

--- ex10/ft_swap.c
+++ ex10/ft_swap.c
@@ -6,7 +6,7 @@
 /*   By: guilhfer <guilhfer@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/25 22:56:07 by guilhfer          #+#    #+#             */
-/*   Updated: 2022/03/26 00:52:30 by guilhfer         ###   ########.fr       */
+/*   Updated: 2022/04/03 17:34:05 by guilhfer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@ void	ft_swap(int *a, int *b)
 	int	left;
 
 	left = *a;
-
 	*a = *b;
 	*b = left;
 }
